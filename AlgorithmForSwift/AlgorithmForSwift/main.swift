9a486d129acc8e88986291e649c6d05c9078b5a9
HyeongSeok <gudtjr051911@gmail.com>
2022-09-15 17:55:05 +0900

    Programmers Level3 베스트 앨범

--- AlgorithmForSwift/AlgorithmForSwift/main.swift
+++ AlgorithmForSwift/AlgorithmForSwift/main.swift
@@ -7,4 +7,35 @@
 
 import Foundation
 
-
+func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
+    var genreDic = [String: Int]()
+    var idDic = [String:[Int]]()
+    
+    for i in stride(from: 0, to: genres.count, by: 1) {
+        if genreDic[genres[i]] != nil {
+            genreDic[genres[i]]! += plays[i]
+        } else {
+            genreDic[genres[i]] = plays[i]
+        }
+        if idDic[genres[i]] != nil {
+            idDic[genres[i]]!.append(i)
+        } else {
+            idDic[genres[i]] = [i]
+        }
+    }
+    
+    let genreRanking: [String] = Array(genreDic.keys).sorted {
+        return genreDic[$0]! > genreDic[$1]!
+    }
+    var answer: [Int] = []
+    for genre in genreRanking {
+        let ids = idDic[genre]!.sorted {
+            return plays[$0] > plays[$1]
+        }
+        answer.append(ids[0])
+        if ids.count > 1 {
+            answer.append(ids[1])
+        }
+    }
+    return answer
+}
