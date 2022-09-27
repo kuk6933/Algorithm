9c66feb89e9dd1fda8f5388807d4c01f7987bf1c
HyeongSeok <gudtjr051911@gmail.com>
2022-09-27 16:48:42 +0900

    Programmers Level2 이진 변환 반복하기

--- Algorithm_swfit/Algorithm_swfit/main.swift
+++ Algorithm_swfit/Algorithm_swfit/main.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
+    var radixS = s
+    var zcnt = 0
+    var cnt = 0
     
-    return []
+    while(radixS.count != 1) {
+        zcnt += radixS.filter({ $0 == "0"}).count
+        radixS = radixS.components(separatedBy: "0").joined()
+        radixS = String(radixS.count, radix: 2)
+        cnt += 1
+    }
+    return [cnt, zcnt]
 }
+
+var a = solution("110")
