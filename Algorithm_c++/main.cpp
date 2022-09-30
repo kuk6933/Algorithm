657372d8aff79ab726411c8278535a279e16f264
HyeongSeok <gudtjr051911@gmail.com>
2022-09-30 10:13:49 +0900

    Programmers Level2 멀리 뛰기

--- Algorithm_c++/main.cpp
+++ Algorithm_c++/main.cpp
@@ -5,39 +5,15 @@
 
 using namespace std;
 
+int solution(int n, int a, int b)
+{
+    int answer = 0;
 
-vector<int> solution(int n, vector<string> words) {
-    vector<int> answer;
-    vector<int> people(11) {1};
-    char pre = words[0][words[0].size()-1];
-    set<string> s;
-    set<string>::iterator iter;
-    s.insert(words[0]);
-    people[1] = 1;
-    int cnt = 2;
-    for(int i=1; i<words.size(); i++) {
-        if (cnt > n) {
-            cnt = 1;
-        }
-        iter = s.find(words[i]);
-        if(iter != s.end() ||
-           words[i][0] != pre) {
-            answer.push_back(cnt);
-            answer.push_back(people[cnt]);
-            return answer;
-        }
-        s.insert(words[i]);
-        people[cnt]++;
-        cnt++;
-        pre = words[i][words[i].size()-1];
+    while(a != b) {
+        a = (a+1) / 2;
+        b = (b+1) / 2;
+        answer++;
     }
-    answer.push_back(0);
-    answer.push_back(0);
-    return answer;
-}
-int main() {
-    vector<string> words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
-    vector<int> a = solution(3,words);
 
-    cout<< a[0] << " " <<a[1];
+    return answer;
 }
---
+++ Algorithm_c++/멀리 뛰기.cpp
@@ -0,0 +1,18 @@
+//
+// Created by ohhyeongseok on 2022/09/30.
+//
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int dp[2001];
+long long solution(int n) {
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i=3; i<=n; i++) {
+        dp[i] = dp[i-1]%1234567 + dp[i-2]%1234567;
+    }
+    return dp[n]%1234567;
+}
