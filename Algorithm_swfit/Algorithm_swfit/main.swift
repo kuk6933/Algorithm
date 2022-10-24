dce3f43b12bfbee7403fae304643f2d0313881f2
HyeongSeok <gudtjr051911@gmail.com>
2022-10-24 10:42:44 +0900

    Programmers Level2 연속 부분 수열 합의 개수

--- Algorithm_swfit/Algorithm_swfit/main.swift
+++ Algorithm_swfit/Algorithm_swfit/main.swift
@@ -1,24 +1,17 @@
 import Foundation
 
-var ans = 0
-var sz = 0
-
-func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
-    sz = dungeons.count
-    let visited = [Bool](repeating: false, count: sz)
-    DFS(visited,dungeons, 0, k)
-    return ans
-}
-
-func DFS(_ visited: [Bool], _ dungeons: [[Int]], _ cnt: Int, _ k: Int) {
-    if cnt > ans {
-        ans = cnt
-    }
-    for i in 0..<sz {
-        if !visited[i] , dungeons[i][0] <= k {
-            var newVisited = visited
-            newVisited[i] = true
-            DFS(newVisited,dungeons, cnt+1, k-dungeons[i][1])
+func solution(_ elements:[Int]) -> Int {
+    var newelements = elements
+    newelements.append(contentsOf: elements)
+    var set = Set<Int>()
+    for i in 1...elements.count {
+        for j in 0..<elements.count {
+            var tmp = 0
+            for k in j..<j+i {
+                tmp += newelements[k]
+            }
+            set.insert(tmp)
         }
     }
+    return set.count
 }
