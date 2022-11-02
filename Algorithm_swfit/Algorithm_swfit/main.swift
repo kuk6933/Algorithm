2e8bd175fd2a72271ca4faf0eb4d3b67f43c986f
HyeongSeok <gudtjr051911@gmail.com>
2022-11-02 16:31:53 +0900

    순열/ 조합

--- Algorithm_swfit/Algorithm_swfit/main.swift
+++ Algorithm_swfit/Algorithm_swfit/main.swift
@@ -1,1 +1,8 @@
+import Foundation
+
+func solution(_ user_id:[String], _ banned_id:[String]) -> Int {
+    var totalSet = Set<Set<Int>>()
+    return 0
+}
+
 
---
+++ Algorithm_swfit/Algorithm_swfit/순열.swift
@@ -0,0 +1,37 @@
+//
+//  순열.swift
+//  Algorithm_swfit
+//
+//  Created by ohhyeongseok on 2022/11/02.
+//
+
+import Foundation
+
+func permutation<T: Comparable>(_ array: [T], _ n: Int) -> [[T]] {
+    var result = [[T]]()
+    
+    if array.count < n { return result }
+    
+    var visited = Array(repeating: false, count: array.count)
+    
+    func cycle(_ now: [T]) {
+        if now.count == n {
+            result.append(now)
+            return
+        }
+        for i in 0..<array.count {
+            if visited[i] {
+                continue
+            } else {
+                visited[i] = true
+                cycle(now + [array[i]])
+                visited[i] = false
+            }
+        }
+    }
+    cycle([])
+    
+    return result
+}
+
+
