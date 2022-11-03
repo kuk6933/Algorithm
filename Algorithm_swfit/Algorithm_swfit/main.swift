2858887288cd5f77d399627b2805df2a921b3fc1
HyeongSeok <gudtjr051911@gmail.com>
2022-11-03 15:38:24 +0900

    Programmers Level3 불량 사용자

--- Algorithm_swfit/Algorithm_swfit/main.swift
+++ Algorithm_swfit/Algorithm_swfit/main.swift
@@ -1,8 +1,1 @@
-import Foundation
-
-func solution(_ user_id:[String], _ banned_id:[String]) -> Int {
-    var totalSet = Set<Set<Int>>()
-    return 0
-}
-
 
---
+++ Algorithm_swfit/Algorithm_swfit/불량 사용자.swift
@@ -0,0 +1,61 @@
+//
+//  불량 사용자.swift
+//  Algorithm_swfit
+//
+//  Created by ohhyeongseok on 2022/11/03.
+//
+import Foundation
+
+func solution(_ user_id:[String], _ banned_id:[String]) -> Int {
+    var bannedList = [[String]]()
+    
+    for bid in banned_id {
+        var matched_id = [String]()
+        var bidArr = bid.map{$0}
+        
+        for uid in user_id {
+            var uidArr = uid.map{$0}
+            if bid.count != uid.count { continue }
+            var flag = true
+            for i in 0..<bidArr.count {
+                if bidArr[i] != uidArr[i] && bidArr[i] != "*" {
+                    flag = false
+                    break
+                }
+            }
+            if flag {
+                matched_id.append(uid)
+            }
+        }
+        bannedList.append(matched_id)
+    }
+    // 각 ban id별로 배열 생성
+    var stack = [(idx: Int, idList: [String])]()
+    var answer = [[String]]()
+    
+    for id in bannedList[0] {
+        stack.append((0, [id]))
+    }
+    
+    while(stack.count > 0) {
+        var cur = stack.removeLast()
+        
+        if cur.idx == banned_id.count - 1 {
+            answer.append(cur.idList)
+            continue
+        }
+        let idx = cur.idx + 1
+        
+        for id in bannedList[idx] {
+            var idList = cur.idList
+            if idList.contains(id) { continue }
+            idList.append(id)
+            stack.append((idx, idList))
+        }
+    }
+    
+    return Set(answer.map { $0.sorted()}).count
+}
+
+print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"]))
+
