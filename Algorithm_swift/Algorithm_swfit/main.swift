bcb2cb99d0de9c810a6c53b4fee82bddc82ab4c2
HyeongSeok <gudtjr051911@gmail.com>
2022-12-15 21:15:06 +0900

    flight tmp

--- Algorithm_swift/Algorithm_swfit/main.swift
+++ Algorithm_swift/Algorithm_swfit/main.swift
@@ -1,1 +1,42 @@
 
+import Foundation
+
+var result = [[String]]()
+
+func dfs(_ cur: String, _ flight: [String: [String]], _ flow: [String], _ cnt: Int,_ collection: Set<String>) {
+    if collection.count == cnt {
+        result.append(flow)
+        return
+    }
+    var collection = collection
+    var flow = flow
+    var flight = flight
+    for nation in flight[cur]! {
+        flow.append(nation)
+        collection.insert(nation)
+        flight[cur]?.remove(at: (flight[cur]?.firstIndex(of: nation))!)
+        dfs(nation, flight, flow, cnt, collection)
+    }
+}
+func solution(_ tickets:[[String]]) -> [String] {
+    var s = Set<String>()
+    var collection = Set<String>()
+    var flight = [String: [String]]()
+    for ticket in tickets {
+        flight[ticket[0]] = []
+        flight[ticket[1]] = []
+        s.insert(ticket[0])
+        s.insert(ticket[1])
+    }
+    for ticket in tickets {
+        flight[ticket[0]]?.append(ticket[1])
+    }
+    for ticket in tickets {
+        flight[ticket[0]]?.sort()
+    }
+    dfs("ICN", flight, ["ICN"], s.count, collection )
+    result = result.sorted(by: { $0[0] < $1[0] })
+    return result[0]
+}
+
+print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]))
