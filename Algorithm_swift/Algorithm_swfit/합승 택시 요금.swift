16c0e4fd4ef2f3dbfb84b6cc356bbb61a29da9b3
HyeongSeok <gudtjr051911@gmail.com>
2022-12-23 21:32:42 +0900

    Programmers Level3 합승 택시 요금

--- Algorithm_swift/Algorithm_swfit/합승 택시 요금.swift
+++ Algorithm_swift/Algorithm_swfit/합승 택시 요금.swift
@@ -1,14 +1,32 @@
 import Foundation
 
 func solution(_ n:Int, _ s:Int, _ a:Int, _ b:Int, _ fares:[[Int]]) -> Int {
-    var visited = [Bool](repeating: false, count: n+1)
-    var roads = [[Int]](repeating: [Int](repeating: 0, count: n+1), count: n+1)
-    for fare in fares {
-        roads[fare[0]][fare[1]] = fare[2]
-        roads[fare[1]][fare[0]] = fare[2]
-    }
-    return 0
-}
+    var graph = [[Int]](repeating: [Int](repeating: 200 * 100000 + 1, count: n + 1), count: n + 1)
+    var ans = Int.max
 
-// S -> A
-// S -> B 인데 같은 길은 한번씩 빼야함 
+    for fare in fares {
+        let from = fare[0]
+        let to = fare[1]
+        let cost = fare[2]
+        
+        graph[from][to] = cost
+        graph[to][from] = cost
+    }
+    for node in 1...n {
+        graph[node][node] = 0
+    }
+
+    for k in 1...n {
+        for i in 1...n {
+            for j in 1...n {
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+            }
+        }
+    }
+
+    for mid in 1...n {
+        ans = min(ans, graph[s][mid] + graph[mid][a] + graph[mid][b])
+    }
+
+    return ans
+}
