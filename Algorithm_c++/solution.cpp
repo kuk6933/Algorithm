ad7451713f5f194b7a0cb0a2f81c24c26ba8dedd
HyeongSeok <gudtjr051911@gmail.com>
2023-01-27 15:32:05 +0900

    swea 1868 파핑파핑 지뢰찾기

--- Algorithm_c++/solution.cpp
+++ Algorithm_c++/solution.cpp
@@ -1,70 +1,62 @@
-struct pair {
-    int x;
-    int y;
-};
+#include <vector>
+#include <utility>
+using namespace std;
 
-pair queue[101];
-int board[11][11];
-int road [11][11];
-bool visited[11][11];
-int sz;
-int front,rear;
-int dx[] = {-1, 0, 1, 0};
-int dy[] = {0, 1, 0, -1};
+vector<vector<pair<int,int>>> v(5);
 
-void enqueue(int y, int x) {
-    pair p;
-    p.y = y;
-    p.x = x;
-    queue[++front] = p;
+void init()
+{
+    v.clear();
 }
 
-pair dequeue() {
-    return queue[++rear];
+void hire(int mID, int mTeam, int mScore)
+{
+    v[mTeam-1].push_back(make_pair(mID, mScore));
 }
 
-void bfs_init(int map_size, int map[10][10]) {
-
-    sz = map_size;
-    for(int i=0; i<10; i++) {
-        for(int j=0; j<10; j++) {
-            road[j+1][i+1] = map[j][i];
+void fire(int mID)
+{
+    for(int i=0; i<5; i++) {
+        for (int j=0; j<v[i].size(); j++) {
+            if(v[i][j].first == mID) {
+                v[i].erase(v[i].begin() + j);
+            }
         }
     }
 }
-int bfs(int x1, int y1, int x2, int y2) {
-    front = -1;
-    rear = -1;
-    for(int i=1; i<11; i++) {
-        for(int j=1; j<11; j++) {
-            visited[j][i] = false;
-            board[j][i] = 0;
-        }
-    }
-    enqueue(y1, x1);
-    visited[y1][x1] = true;
 
-    while(rear != front) {
-        pair cur = dequeue();
-        if(cur.y == y2 && cur.x == x2) {
-            return board[y2][x2];
-        }
-        for(int i=0; i<4; i++) {
-            int nx = cur.x + dx[i];
-            int ny = cur.y + dy[i];
-            if(nx <= 0 || ny <= 0 || nx>sz || ny > sz) {
-                continue;
+void updateSoldier(int mID, int mScore)
+{
+    for(int i=0; i<5; i++) {
+        for (int j=0; j<v[i].size(); j++) {
+            if(v[i][j].first == mID) {
+                v[i][j].second = mScore;
             }
-            if(visited[ny][nx]) {
-                continue;
-            }
-            if(road[ny][nx] == 1) {
-                continue;
-            }
-            visited[ny][nx] = true;
-            board[ny][nx] = board[cur.y][cur.x]+1;
-            enqueue(ny ,nx);
         }
     }
-    return -1;
+}
+
+void updateTeam(int mTeam, int mChangeScore)
+{
+    for(int i=0; i<v[mTeam].size(); i++){
+        v[mTeam][i].second += mChangeScore;
+        if(v[mTeam][i].second > 5) {
+            v[mTeam][i].second= 5;
+        } else if (v[mTeam][i].second < 1) {
+            v[mTeam][i].second = 1;
+        }
+    }
+}
+
+int bestSoldier(int mTeam)
+{
+    int bestScore = 0;
+    int Id;
+    for(int i=0; i<v[mTeam].size(); i++) {
+        if(v[mTeam][i].second > bestScore) {
+            bestScore = v[mTeam][i].second;
+            Id = v[mTeam][i].first;
+        }
+    }
+    return Id;
 }
