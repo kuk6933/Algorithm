cf7d0b3f798cca65a5f359375f75f23f650123aa
HyeongSeok <gudtjr051911@gmail.com>
2023-02-08 16:50:53 +0900

    swea 촛불 이벤트

--- Algorithm_c++/main.cpp
+++ Algorithm_c++/main.cpp
@@ -1,57 +1,26 @@
 #include<iostream>
 #include <vector>
 using namespace std;
-
+#define MAX 1000000000000000001
 int main(int argc, char** argv)
 {
     int test_case;
     int T;
-
     cin>>T;
-
+    vector<unsigned long long> v;
+    unsigned long long i= 1;
+    while(1) {
+        if( i*(i+1) / 2 >=MAX) {
+            break;
+        }
+        v.push_back(i*(i+1) / 2);
+        i++;
+    }
     for(test_case = 1; test_case <= T; ++test_case)
     {
-        vector<int> v(1000001);
-        for(int i=0; i<1000001; i++) {
-            v[i] = 0;
-        }
-        int n,p;
-        cin>>n>>p;
-        int last;
-        for(int i=0; i<n; i++) {
-            int study;
-            cin>>study;
-            last = study;
-            v[study] = 1;
-        }
-        int l,r;
-        l = 0;
-        r = -1;
-
-        int maxDistance = 0;
-        int distance = 0;
-        while(1) {
-            if(r >= last && p == 0) {
-                break;
-            }
-            if(p >= 0) { // 해킹 가능
-                r++;
-                if(v[r] == 0) {
-                    p--;
-                }
-                distance++;
-                if(distance > maxDistance && p >= 0) {
-                    maxDistance = distance;
-                }
-            } else { // 불가
-                if(v[l] == 0) {
-                    p++;
-                }
-                l++;
-                distance--;
-            }
-        }
-        cout<<"#"<<test_case<<" "<<maxDistance<<"\n";
+        unsigned long long n;
+        cin>>n;
+        cout<<"1";
     }
     return 0;
 }
---
+++ Algorithm_c++/촛불 이벤트.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int test_case;
+    int T;
+    cin>>T;
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        long long n;
+        cin>>n;
+        n *= 2;
+        long long l = 0;
+        long long r = sqrt(n);
+        long long m;
+        long long ans = 0;
+        while(1) {
+            if(l > r ) {
+                break;
+            }
+            m = (l+r) / 2;
+            if(m*(m+1) == n) {
+                ans = m;
+                break;
+            } else {
+                if(n< m*(m+1)) {
+                    r = m-1;
+                } else {
+                    l = m+1;
+                }
+            }
+        }
+        if(ans ==0) {
+            cout<<"#"<<test_case<<" "<< -1<<"\n";
+        }else {
+            cout<<"#"<<test_case<<" "<< ans<<"\n";
+        }
+    }
+    return 0;
+}
