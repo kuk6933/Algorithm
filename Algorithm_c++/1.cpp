7a9201f11fcfde786324e0d79cae9b1b8230806d
HyeongSeok <gudtjr051911@gmail.com>
2023-02-15 01:14:54 +0900

    swea segment tree 연습2

--- Algorithm_c++/1.cpp
+++ Algorithm_c++/1.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int times;
     cin>>times;
     for (int t=1; t<=times; t++) {
