4dc0138ae6f8caeaf333793208f46bae0eed2386
HyeongSeok <gudtjr051911@gmail.com>
2023-03-30 16:59:32 +0900

    Linked List

--- Algorithm_c++/main.cpp
+++ Algorithm_c++/main.cpp
@@ -1,251 +1,49 @@
-#include <queue>
-#include <vector>
-#define MAX_N 100
-
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-
-#include <stdio.h>
-
-#define MAX_N 100
-
-extern void init(int N, int mDish[MAX_N][MAX_N]);
-extern int dropMedicine(int mTarget, int mRow, int mCol, int mEnergy);
-extern int cleanBacteria(int mRow, int mCol);
-
-#define CMD_INIT 100
-#define CMD_DROP 200
-#define CMD_CLEAN 300
-
-static bool run()
-{
-    int query_num;
-    scanf("%d", &query_num);
-
-    int ret, ans;
-    bool ok = false;
-    static int dish[MAX_N][MAX_N];
-
-    for (int q = 0; q < query_num; q++)
-    {
-
-        int query;
-        scanf("%d", &query);
-
-        if (query == CMD_INIT)
-        {
-            int N;
-            scanf("%d", &N);
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
-                {
-                    scanf("%d", &dish[i][j]);
-                }
-            }
-
-            init(N, dish);
-            ok = true;
-        }
-        else if (query == CMD_DROP)
-        {
-            int mTarget;
-            int mRow, mCol;
-            int mEnergy;
-            scanf("%d %d %d %d", &mTarget, &mRow, &mCol, &mEnergy);
-            ret = dropMedicine(mTarget, mRow, mCol, mEnergy);
-            scanf("%d", &ans);
-            if (ans != ret)
-            {
-                ok = false;
-            }
-        }
-        else if (query == CMD_CLEAN)
-        {
-            int mRow, mCol;
-            scanf("%d %d", &mRow, &mCol);
-            ret = cleanBacteria(mRow, mCol);
-            scanf("%d", &ans);
-            if (ans != ret)
-            {
-                ok = false;
-            }
-        }
-    }
-    return ok;
-}
-
-int main()
-{
-    setbuf(stdout, NULL);
-    int T, MARK;
-    scanf("%d %d", &T, &MARK);
-
-    for (int tc = 1; tc <= T; tc++)
-    {
-        int score = run() ? MARK : 0;
-        printf("#%d %d\n", tc, score);
-    }
-    return 0;
-}
+#include <iostream>
+#define MAX 200
 
 using namespace std;
 
-struct Pos {
-    int r; // y
-    int c; // x
-};
+int arr[MAX] = {1, 2, 3, 4, 5, 6 ,7 ,8, 9};
+int segmentTree[MAX*4];
 
-struct Cell {
-    int target = -1;
-    int energy = -1;
-    int version = 0;
-    Pos pos;
-};
+int make_segmentTree(int node, int start, int end) {
+    if(start == end) return segmentTree[node] = arr[start];
 
-struct cmp {
-    bool operator()(Cell a, Cell b) {
-        if(a.energy == b.energy) {
-            if(a.pos.r == b.pos.r) {
-                return a.pos.c > b.pos.c;
-            } else {
-                return a.pos.r > b.pos.r;
-            }
-        } else {
-            return a.energy < b.energy;
-        }
-    }
-};
+    int mid = (start + end) /2;
+    int left_result = make_segmentTree(node*2, start, mid);
+    int right_result = make_segmentTree(node * 2 + 1, mid+1, end);
+    return segmentTree[node] = left_result + right_result;
+}
 
-const int dr[] = { -1,0,0, 1 };
-const int dc[] = { 0,-1,1,0 };
-int n;
-int version;
+int sum(int node, int start, int end, int left, int right) { //node: 현재 위치, start, end: 현재 노드의 범위, left, right: 찾고자 하는 범위
+    if(left > end || right < start) return 0;
+    if(left <= start && right >= end) return segmentTree[node];
 
-Cell dish[MAX_N+1][MAX_N+1];
+    int mid = (left + right) / 2;
+    int left_result = sum(node*2, start, mid, left, right);
+    int right_result = sum(node*2+1, mid+1, end, left, right);
 
-priority_queue<Cell, vector<Cell>, cmp> pq;
-int target_num[3];
+    return left_result + right_result;
+}
 
-void init(int N, int mDish[MAX_N][MAX_N])
-{
-    n = N;
-    version = 0;
-    target_num[1] = target_num[2] = 0;
-    for(int i=0; i<N; i++) {
-        for(int j=0; j<N; j++) {
-            dish[i+1][j+1].energy = mDish[i][j];
-            dish[i+1][j+1].pos = {i+1, j+1};
-            dish[i+1][j+1].version = 0;
-            dish[i+1][j+1].target = 0;
-        }
+void update(int node, int start, int end, int idx, int diff) {
+    if(idx > end || idx < start) return;
+    segmentTree[node] += diff;
+
+    if(start != end) {
+        int mid = (start + end) / 2;
+        update(node * 2, start, mid, idx, diff);
+        update(node * 2 + 1, start, mid, idx, diff);
     }
 }
 
-int dropMedicine(int mTarget, int mRow, int mCol, int mEnergy)
-{
-    version++;
-    if(dish[mRow][mCol].target == 0) { // empty
-        target_num[mTarget]++;
-        mEnergy -= dish[mRow][mCol].energy;
-        dish[mRow][mCol].target = mTarget;
-        dish[mRow][mCol].version = version;
-    } else if(dish[mRow][mCol].target == mTarget) { // 같은 종류 세균
-        dish[mRow][mCol].version = version;
-    } else {
-        return target_num[mTarget];
-    }
-
-    queue<Pos> q;
-    pq = {};
-    q.push(dish[mRow][mCol].pos);
-    while(!q.empty()) {
-        Pos cur = q.front(); q.pop();
-        int cr = cur.r;
-        int cc = cur.c;
-        for(int i=0; i<4; i++) {
-            int nr = cr + dr[i];
-            int nc = cc + dc[i];
-
-            if(nr <= 0 || nc <= 0 || nr > n || nc > n) continue;
-            if(dish[nr][nc].version >= version) continue;
-            if(dish[nr][nc].target!= 0 && dish[nr][nc].target != mTarget) continue;
-            dish[nr][nc].version = version;
-            if(dish[nr][nc].target == mTarget) {
-                q.push(dish[nr][nc].pos);
-            } else {
-                pq.push(dish[nr][nc]);
-            }
-        }
-    }
-    //2단계 -- pq번식 가능한 위치 넣어놨음
-
-    while(mEnergy > 0) {
-        if(pq.empty()) break;
-        Cell now = pq.top(); pq.pop();
-        dish[now.pos.r][now.pos.c].version = version;
-        dish[now.pos.r][now.pos.c].target = mTarget;
-        target_num[mTarget]++;
-        mEnergy -= now.energy;
-
-        q.push(now.pos);
-        while(!q.empty()) {
-            Pos cur = q.front(); q.pop();
-            int cr = cur.r;
-            int cc = cur.c;
-            for(int i=0; i<4; i++) {
-                int nr = cr + dr[i];
-                int nc = cc + dc[i];
-
-                if(nr <= 0 || nc <= 0 || nr > n || nc > n) continue;
-                if(dish[nr][nc].version >= version) continue;
-                if(dish[nr][nc].target!= 0 && dish[nr][nc].target != mTarget) continue;
-                dish[nr][nc].version = version;
-                if(dish[nr][nc].target == mTarget) {
-                    q.push(dish[nr][nc].pos);
-                } else {
-                    pq.push(dish[nr][nc]);
-                }
-            }
-        }
-    }
-    return target_num[mTarget];
-}
-
-int cleanBacteria(int mRow, int mCol)
-{
-    version++;
-    if(dish[mRow][mCol].target == 0) {
-        return -1;
-    }
-    dish[mRow][mCol].version = version;
-    Cell start = dish[mRow][mCol];
-    queue<Pos> q;
-    q.push(start.pos);
-    while(!q.empty()) {
-        Pos cur = q.front(); q.pop();
-        int cr = cur.r;
-        int cc = cur.c;
-        for(int i=0; i<4; i++) {
-            int nr = cr + dr[i];
-            int nc = cc + dc[i];
-
-            if(nr <= 0 || nc <= 0 || nr > n || nc > n) continue;
-            if(dish[nr][nc].version >= version) continue;
-            if(dish[nr][nc].target != start.target) continue;
-            dish[nr][nc].version = version;
-            q.push(dish[nr][nc].pos);
-        }
-    }
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            if(dish[i][j].version >= version) {
-                dish[i][j].version = 0;
-                target_num[dish[i][j].target]--;
-                dish[i][j].target = 0;
-            }
-        }
-    }
-    return target_num[start.target];
+int main() {
+    int n;
+    cin>>n;
+    make_segmentTree(1, 0, n - 1);
+    int Index = 1;
+    int Value = 5;
+    int Diff = Value - arr[Index];
+    arr[Index] = Value;
+    update(1, 0, n - 1, Index, Diff);
 }
