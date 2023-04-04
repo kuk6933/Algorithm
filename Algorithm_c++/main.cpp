2c32bc1f08ae044cca475f72c503ffd7e6dc6d37
HyeongSeok <gudtjr051911@gmail.com>
2023-04-04 14:33:32 +0900

    백준 시험감독 c++

--- Algorithm_c++/main.cpp
+++ Algorithm_c++/main.cpp
@@ -5,4 +5,30 @@
 using namespace std;
 
 int main() {
+    int n;
+    long long ans=0;
+    cin>>n;
+    vector<int> v;
+    int num;
+    for(int i=0; i <n; i++) {
+        cin>>num;
+        v.push_back(num);
+    }
+    int b,c;
+    cin>>b>>c;
+    for(int i=0; i<v.size(); i++) {
+        v[i]-= b;
+        ans++;
+        if(v[i]> 0) {
+            if(v[i] <= c) {
+                ans++;
+            } else {
+                ans += v[i]/ c;
+                if(v[i]%c >0) {
+                    ans++;
+                }
+            }
+        }
+    }
+    cout<<ans;
 }
