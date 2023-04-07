b976312425e186ae226da85f829ee0d6991bfa6d
HyeongSeok <gudtjr051911@gmail.com>
2023-04-08 03:38:36 +0900

    c++ 상어중학교

--- Algorithm_c++/상어 초등학교.cpp
+++ Algorithm_c++/상어 초등학교.cpp
@@ -9,17 +9,17 @@ int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
 
 struct PRIORITY {
-    int empty = 0;
-    int y = 22;
-    int x = 22,
-            friend_num = 0;
+    int empty = 0; //현재 가장 적합한 칸의 주위 빈칸 개수
+    int y = 22; // 좌표
+    int x = 22; //좌표
+    int  friend_num = 0; // 친구 숫자
 };
 
 struct DESK {
-    int empty = 4;
-    bool can = true;
-    int student=0;
-    vector<int> friends;
+    int empty = 4; //이 자리 주변 빈칸 개수
+    bool can = true; //자리에 앉을 수 있는지(사람이 없는지)
+    int student=0; //자리에 앉은 학생 번호
+    vector<int> friends; //주변에 앉은 친구들 번호
 };
 
 
@@ -27,12 +27,12 @@ struct DESK {
 int main() {
     int n;
     DESK classroom[21][21];
-    vector<vector<int>> like(401);
-    vector<int> line;
+    vector<vector<int>> like(401); // 좋아하는 학생 관리를 위한 벡터
+    vector<int> line; //앉을 순서
     cin>>n;
     for(int i=1; i<=n; i++) {
         for(int j=1; j<=n; j++) {
-            if(i==1 || j==1 || i==n || j== n) {
+            if(i==1 || j==1 || i==n || j== n) { // 모서리부분 따로 계산해줍니다
                 classroom[i][j].empty = 3;
             }
             if(i==1 && j==1) {
@@ -67,15 +67,15 @@ int main() {
         PRIORITY best;
         for(int i=1; i<=n; i++) {
             for(int j=1; j <=n; j++) {
-                if(classroom[i][j].can == false) continue;
+                if(classroom[i][j].can == false) continue; //자리에 사람이 있으면
                 int cnt = 0;
-                for(int k=0; k<classroom[i][j].friends.size(); k++) {
+                for(int k=0; k<classroom[i][j].friends.size(); k++) { //친구 수 세기
                     for(int t=0; t<like[turn].size(); t++) {
                         if(classroom[i][j].friends[k] == like[turn][t]) cnt++;
                     }
                 }
                 bool change = false;
-                if(best.friend_num < cnt) {
+                if(best.friend_num < cnt) { //우선순위 비교 하는 과정입니다
                     change = true;
                 } else if(best.friend_num == cnt) {
                     if(best.empty < classroom[i][j].empty) {
@@ -90,7 +90,7 @@ int main() {
                         }
                     }
                 }
-                if(change) {
+                if(change) { //가장 적합한 자리 바뀜
                     best.friend_num = cnt;
                     best.y=i;
                     best.x=j;
@@ -99,10 +99,10 @@ int main() {
             }
         }
 
-        // 자리 fix했어
+        // 학생이 자리에 앉음
         classroom[best.y][best.x].can = false;
         classroom[best.y][best.x].student = turn;
-        for(int i=0; i<4; i++) {
+        for(int i=0; i<4; i++) { //주변 자리들의 빈칸 개수를 1씩 줄여주고 친구에 자신을 넣어줍니다
             int ny = best.y+dy[i];
             int nx = best.x+dx[i];
             if(ny <= 0 || nx <= 0 || ny >n || nx >n)continue;
--- Algorithm_c++/상어중학교.cpp
+++ Algorithm_c++/상어중학교.cpp
@@ -134,7 +134,7 @@ int main() {
         for(int c=1; c<=3; c++) {
             for(int i=0; i<n; i++) {
                 for(int j=0; j<n; j++) {
-                   tmp[i][j] = map[n-j-1][i];
+                    tmp[i][j] = map[n-j-1][i];
                 }
             }
             for(int i=0; i<n; i++) {
