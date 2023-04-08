23015348f79da03cc68daf6a472aa0981e33214c
HyeongSeok <gudtjr051911@gmail.com>
2023-04-08 18:50:01 +0900

    주사위 굴리기 2

--- Algorithm_c++/main.cpp
+++ Algorithm_c++/main.cpp
@@ -4,121 +4,109 @@
 
 using namespace std;
 
-int dy[] = {-2,0,-1,-1,-1,0,1,1,1};
-int dx[] = {-2,-1,-1,0,1,1,1,0,-1};
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
 
-pair<int,int> around[] = {{-1,-1}, {-1, 1}, {1, 1}, {1, -1} };
-//int dy[] = {-1, 0, 1, 0};
-//int dx[] = {0, 1, 0, -1};
+int dice[7] = {0,1,2,3,4,5,6}; //위 앞 오른쪽 왼쪽 뒤 밑
+
+void update(int d) {
+
+    int d1= dice[1];
+    int d2= dice[2];
+    int d3= dice[3];
+    int d4= dice[4];
+    int d5= dice[5];
+    int d6= dice[6];
+    if(d==0) {
+        dice[1]=d5;
+        dice[5]=d6;
+        dice[6]=d2;
+        dice[2]=d1;
+    } else if(d== 1) {
+        dice[1]=d4;
+        dice[4]=d6;
+        dice[6]=d3;
+        dice[3]=d1;
+    } else if(d== 2) {
+        dice[1]=d2;
+        dice[2]=d6;
+        dice[6]=d5;
+        dice[5]=d1;
+    } else if(d == 3) {
+        dice[4]=d1;
+        dice[1]=d3;
+        dice[3]=d6;
+        dice[6]=d4;
+    }
+}
 
 
-vector<pair<int,int>> movement;
-
-int tmp[51][51];
-int map[51][51];
-int cloud[51][51];
+int n,m,k;
+int map[21][21];
 
 int main() {
-    int n, m;
-    cin>>n>>m;
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            cin>>map[i][j];
-        }
-    }
-
-
-    for(int i=0; i<m; i++) {
-        int d,s;
-        cin>>d>>s;
-        movement.push_back(make_pair(d,s));
-    }
-
-    cloud[n][1] = 1;
-    cloud[n][2] = 1;
-    cloud[n-1][1] = 1;
-    cloud[n-1][2] = 1;
-
-    for(int t=0; t<m; t++) {
-
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n; j++) {
-                tmp[i][j] = 0;
-            }
-        }
-
-        int dir = movement[t].first;
-        int speed = movement[t].second;
-        speed %= n;
-
-        //구름 움직이기
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n; j++) {
-                int ny = i+dy[dir]*speed;
-                int nx = j+dx[dir]*speed;
-                if(ny <=0) ny += n;
-                if(nx <=0) nx+=n;
-                if(ny>n) ny-=n;
-                if(nx>n) nx-=n;
-                tmp[ny][nx] = cloud[i][j];
-            }
-        }
-
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n; j++) {
-                cloud[i][j] = tmp[i][j];
-            }
-        }
-
-        //2단계
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n; j++) {
-                if(cloud[i][j]==1) {
-                    map[i][j]++;
-                }
-            }
-        }
-
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n; j++) {
-
-                if(cloud[i][j]==1) {
-                    int cnt= 0;
-                    for(int k=0; k<4; k++) {
-                        int ny = i + around[k].first;
-                        int nx = j + around[k].second;
-                        if(ny <=0 || nx <= 0 || ny>n || nx >n) continue;
-                        if(map[ny][nx] > 0) {
-                            cnt++;
-                        }
-                    }
-                    map[i][j]+= cnt;
-                }
-            }
-        }
-
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n; j++) {
-                if(cloud[i][j] == 1) {
-                    cloud[i][j]= 0;
-                    continue;
-                }
-                if(map[i][j]>= 2) {
-                    cloud[i][j] = 1;
-                    map[i][j] -=2;
-                }
-            }
-        }
-
-    }
+    int direction = 1;
     int sum = 0;
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            if(map[i][j]>0) {
-                sum+=map[i][j];
-            }
-        }
-    }
+    cin>>n>>m>>k;
+  for(int i=0; i<n; i++) {
+      for(int j=0; j<m; j++) {
+          cin>>map[i][j];
+      }
+  }
 
-    cout<<sum;
+  pair<int,int> cur = {0,0};
+  for(int K=0; K<k; K++) {
+      int next_y = cur.first + dy[direction];
+      int next_x = cur.second + dx[direction];
+
+      if(next_y <0 || next_x <0 || next_y >=n || next_x >= m) {
+          direction +=2;
+          direction %= 4;
+      }
+      cur.first+= dy[direction];
+      cur.second+= dx[direction];
+
+      update(direction);
+
+      int num = map[cur.first][cur.second];
+      bool visited[21][21];
+      for(int i=0; i<n; i++) {
+          for(int j=0; j<m; j++) {
+              visited[i][j] = false;
+          }
+      }
+ //점수획득
+      queue<pair<int,int>> q;
+      int sz = 1;
+      q.push({cur.first, cur.second});
+      visited[cur.first][cur.second] = true;
+      while(!q.empty()) {
+          int curY = q.front().first;
+          int curX = q.front().second;
+          q.pop();
+          for(int i=0; i<4; i++) {
+              int ny = curY +dy[i];
+              int nx = curX + dx[i];
+              if(ny <0 || nx <0 || ny >=n || nx >= m) continue;
+              if(visited[ny][nx]) continue;
+              if(map[ny][nx]== num) {
+                  q.push({ny,nx});
+                  visited[ny][nx]= true;
+                  sz++;
+              }
+          }
+      }
+      sum += num * sz;
+
+      if(dice[6] > num) {
+          direction++;
+          direction %= 4;
+      } else if(dice[6] < num) {
+          direction--;
+          if(direction<0) {
+              direction+=4;
+          }
+      }
+  }
+  cout<<sum;
 }
